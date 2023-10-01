220ab13703a8b7e1a1284e3d0667b68b6d1db7ae
jbiccler <jaron.biccler@gmail.com>
2023-10-01 16:02:10 +0200

    feat: chapter 12 complete

---
+++ chapter12/summary_q5/src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void sort2(int &x, int &y) {
+  if (x > y) {
+    auto tmp = x;
+    x = y;
+    y = tmp;
+  };
+}
+
+int main() {
+  int x{7};
+  int y{5};
+
+  sort2(x, y);
+  std::cout << x << ' ' << y << '\n'; // should print 5 7
+
+  sort2(x, y);
+  std::cout << x << ' ' << y << '\n'; // should print 5 7
+
+  return 0;
+}
